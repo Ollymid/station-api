aab8a22e3941b081c8a1ab6c51aad53088ad0fdb
Ollymid <ollymiddleton@hotmail.co.uk>
2019-07-04 11:23:04 +0100

    a nice table of trains ordered by train arrival time

--- src/components/TrainTimes.js
+++ src/components/TrainTimes.js
@@ -21,17 +21,46 @@ const useFetch = (url) => {
 
 const TrainTimes = ()  => {
   const res = useFetch("https://api.tfl.gov.uk/StopPoint/940GZZLUGPS/Arrivals?mode=tube");
+  if (!res.data) {
+  return <div>
+          Loading data...
+        </div>
+}
   const trainList = Array.isArray(res.data)
-  console.log(trainList);
   function sortByTime(a, b) {
     return a.timeToStation - b.timeToStation;
   };
   const sortedTrains = trainList && res.data.sort(sortByTime)
-  console.log(sortedTrains);
+
+  const table = sortedTrains.map(train => {
+  return (
+    <tr key={train.id}>
+      <td className={train.lineId}>{train.lineName}</td>
+      <td className={!train.destinationName?"nis":""}>
+        {train.destinationName ? train.destinationName : "Not in Service"}
+      </td>
+      <td>{Math.round(train.timeToStation/60)} Minutes</td>
+    </tr>
+    )
+  });
   return (
     <section className="dispay-flex flex-container__column">
-      <h2>Please see today's train schedule for this station down below1</h2>
+      <h2>Please see today's train schedule for this station down below</h2>
 
+      <div className="flex-container__row">
+        <table align="center" className="train-table">
+          <thead>
+            <tr>
+              <th>Line</th>
+              <th>Destination</th>
+              <th>Time to Arrival</th>
+            </tr>
+          </thead>
+          <tbody>
+            {table}
+          </tbody>
+        </table>
+      </div>
 
     </section>
   )
