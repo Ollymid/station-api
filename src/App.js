6c0540238c1b22f9d15ca016b8a2cb32aaf7c7a7
Ollymid <ollymiddleton@hotmail.co.uk>
2019-07-04 10:35:03 +0100

    adds in train times component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import TrainTimes from './components/TrainTimes';
 
 function App() {
   return (
     <div>
       <Header />
+      <TrainTimes />
       <Footer />
     </div>
   );
---
+++ src/components/TrainTimes.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TrainTimes = ()  => {
+
+
+  return (
+    <section className="dispay-flex flex-container__column">
+      <h2>Please see today's train schedule for this station down below1</h2>
+    </section>
+  )
+}
+
+export default TrainTimes
