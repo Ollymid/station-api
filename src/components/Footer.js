c82d6ad06ee3e9f1de2454697a57236dbc2c4b38
Ollymid <ollymiddleton@hotmail.co.uk>
2019-07-04 10:03:00 +0100

    adds header and footer to page

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = ()  => {
+  return (
+     <footer className="app-footer">
+        <h5> Made with &hearts; by Olly Middleton</h5>
+     </footer>
+  )
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = ()  => {
+  return (
+     <header className="app-header">
+        <h1>Great Portland Street Train Times</h1>
+     </header>
+  )
+}
+
+export default Header;
